da59ed0c15ab863de07ddb90411fe595764a86bf
clementRTB2 <cmcolombat@gmail.com>
2024-06-21 10:43:11 +0200

    create user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,12 +1,43 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
-const UserSchema = new mongoose.Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, enum: ['customer', 'admin', 'staff'], default: 'customer' },
-  createdAt: { type: Date, default: Date.now }
-});
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      unique: true
+    },
+    firstName: {
+      type: String,
+      required: true
+    },
+    lastName: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    role: {
+      type: String,
+      enum: ['user', 'admin', 'customer', 'staff'],
+      default: 'customer'
+    }
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('User', UserSchema);
+// Compare the given password with the hashed password in the database
+userSchema.methods.comparePassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
